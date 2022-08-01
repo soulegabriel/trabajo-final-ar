80dd390dbc976fe7e6bce06f38c824f7fa8dc62a
soulegabriel <soulegabriel@hotmail.com>
2022-08-01 15:42:22 -0300

    subida3

---
+++ js/javas.js
@@ -0,0 +1,14 @@
+function ConsultarSalida(){
+    var respuesta= confirm('¿Estas seguro que deseas salir?');
+    if (respuesta == true){
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+
+
+
+
